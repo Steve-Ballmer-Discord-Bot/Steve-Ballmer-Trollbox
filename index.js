da2af70f6b5a3845b2454f21df133420b9af14d5
RoboticObsession <64986112+RoboticObsession@users.noreply.github.com>
2021-01-10 00:10:26 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+const tb = require("tb93");
+const fs = require("fs");
+
+let bot = new tb.Trollbox(new tb.User("Steve Ballmer"),true);
+let prefix = "!";
+
+bot.on_message = msg => {
+  var command = msg.content.toLowerCase();
+  if (command === "Steve Ballmer") {
+  fs.readFile(__dirname + "/commands/mention.txt", 'utf8' , (err, data) => {
+  if (err) {
+    bot.send("Error:\n" + err.toString())
+  }
+  bot.send(data);
+  })
+  }
+
+  if (command === prefix + "help") {
+  fs.readFile(__dirname + "/commands/help.txt", 'utf8' , (err, data) => {
+  if (err) {
+    bot.send("Error:\n" + err.toString())
+  }
+  bot.send(data);
+  })
+  }
+
+  if (command === prefix + "botinfo") {
+  fs.readFile(__dirname + "/commands/botinfo.txt", 'utf8' , (err, data) => {
+  if (err) {
+    bot.send("Error:\n" + err.toString())
+  }
+  bot.send(data);
+  })
+  }
+
+  if (command === prefix + "news") {
+  fs.readFile(__dirname + "/commands/news.txt", 'utf8' , (err, data) => {
+  if (err) {
+    bot.send("Error:\n" + err.toString())
+  }
+  bot.send(data);
+  })
+  }
+
+  if (command === prefix + "ping") {
+    bot.send("So, this awfull, the ping command not works very well so we disable it.");
+  }  
+};
+
+bot.on_error = err => { bot.send("Error:\n" + err.toString()) }
+
+bot.connect();
+console.log("Bot online!")
